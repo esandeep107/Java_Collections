a4a7a107ea634dec96718fc175f27e146350e559
sayaka-tamura <arccycle@gmail.com>
2019-05-07 22:20:43 +0900

    Add compareToTest class

--- test/com/in28minutes/collections/StudentTest.java
+++ test/com/in28minutes/collections/StudentTest.java
@@ -6,14 +6,37 @@
  */
 package com.in28minutes.collections;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class StudentTest {
-	Student stu = new Student(1, "Ranga");
+	Student stu = new Student(2, "Eve");
 
-	@Test
-	void compareToTest() {
-		stu.compareTo(new Student(100, "Adam"));
+	@Nested
+	@DisplayName("For compareTo method")
+	class compareToTest {
+
+		@BeforeEach
+		void beforeEach(TestInfo info) {
+			System.out.println("Initializing Data..." + info.getDisplayName());
+		}
+
+		@Test
+		@DisplayName("comparing larger ID number")
+		void compareToLargeNumID() {
+			assertTrue(stu.compareTo(new Student(100, "Adam")) < 1);
+		}
+
+		@Test
+		@DisplayName("comparing smaller ID number")
+		void compareToSmallNumID() {
+			assertTrue(stu.compareTo(new Student(1, "Ranga")) > -1);
+		}
 	}
 
 }

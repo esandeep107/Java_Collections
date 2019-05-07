0dc5bcd41d2987e98f75235a21a4e0bd281fcfe8
sayaka-tamura <arccycle@gmail.com>
2019-05-07 21:14:15 +0900

    Add testTrue_SizeOf_List_after_removeEndWith() Test

--- test/com/in28minutes/collections/ArrayListRunnerTest.java
+++ test/com/in28minutes/collections/ArrayListRunnerTest.java
@@ -7,22 +7,27 @@
 package com.in28minutes.collections;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ArrayListRunnerTest {
 
-	ArrayListRunner alIterator = new ArrayListRunner();
-	Collection actualWords = new ArrayList(Arrays.asList("Apple", "Bat", "Orange", "Cat"));
+	ArrayListRunner alIterator = new ArrayListRunner();;
 
 	@BeforeAll
 	static void beforeAll() {
@@ -40,11 +45,29 @@ class ArrayListRunnerTest {
 	}
 
 	@Test
-	void testremoveEndWith_at() {
+	@DisplayName("When a word end with \"at\", remove the word")
+	void testremoveEndWith_at_2atWord() {
 		// can make these list as below in-line
+
+		Collection actualWords = new ArrayList(Arrays.asList("Apple", "Bat", "Orange", "Cat"));
+
 		actualWords = alIterator.removeEndWith_at((List<String>) actualWords);
 		List<String> expectedvalue = Arrays.asList("Apple", "Orange");
 		assertArrayEquals(expectedvalue.toArray(), actualWords.toArray());
+	}
+
+	@DisplayName("check true when a list length > 0")
+	@ParameterizedTest
+	@MethodSource("generateData")
+	void testTrue_SizeOf_List_after_removeEndWith(List<String> actualValue) {
+		actualValue = alIterator.removeEndWith_at(actualValue);
+		assertTrue(actualValue.size() > 0);
+	}
+
+	static Stream<Arguments> generateData() {
+		return Stream.of(Arguments.of(new ArrayList(Arrays.asList("Apple", "Bat", "Orange", "Cat"))),
+				// Arguments.of(new ArrayList(Arrays.asList("Pat", "Boat", "Seat"))),
+				Arguments.of(new ArrayList(Arrays.asList("Apple", "Banana", "Orange"))));
 	}
 
 }

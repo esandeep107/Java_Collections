73c5138ca167ed9b773c19f55aa2f14398d61e75
sayaka-tamura <arccycle@gmail.com>
2019-05-08 11:47:14 +0900

    Add SetRunner class

---
+++ src/com/in28minutes/collections/SetRunner.java
@@ -0,0 +1,24 @@
+/**
+ * @author : Sayaka Tamura
+ * May 8, 2019
+ * Udemy Java Collections 2
+ * https://www.youtube.com/watch?v=-54t79mEdNk&list=WL&index=6
+ */
+
+/**
+ * List: 
+ * <Basic> Set does not particularly maintain insertion order but, Linked HashSet maintains the insertion order.
+ * <Duplication> The add() method returns false if you try to insert the duplicate elements.
+ * <Methods> Set does not define any additional method.
+ * <Implementation> Set is implemented by HashSet, LinkedHashSet, EnumSet, TreeSet, CopyOnWriteArraySet.
+ */
+
+package com.in28minutes.collections;
+
+public class SetRunner {
+
+	public static void main(String[] args) {
+
+	}
+
+}

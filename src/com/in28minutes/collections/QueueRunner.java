a3e234da31c3a2dc8453f1e5179a9c5e8387acbd
sayaka-tamura <arccycle@gmail.com>
2019-05-08 14:15:53 +0900

    Add StringLengthComparator method

---
+++ src/com/in28minutes/collections/QueueRunner.java
@@ -0,0 +1,38 @@
+/**
+ * @author : Sayaka Tamura
+ * May 8, 2019
+ * Udemy Java Collections 3
+ * https://www.youtube.com/watch?v=114BHPiaPGc&list=WL&index=7
+ */
+package com.in28minutes.collections;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class StringLengthComparator implements Comparator<String> {
+	@Override
+	public int compare(String value1, String value2) {
+		return Integer.compare(value1.length(), value2.length());
+	}
+
+}
+
+public class QueueRunner {
+	public static void main(String[] args) {
+		Queue<String> queue = new PriorityQueue<>(new StringLengthComparator());
+		// queue.offer("Apple");
+
+		queue.addAll(List.of("Zebra", "Monkey", "Cat"));
+		System.out.println(queue);
+
+		System.out.println(queue.poll()); // Remove the head of this queue from the list
+		System.out.println(queue);
+		System.out.println(queue.poll()); // Remove the head of this queue from the list
+		System.out.println(queue);
+		System.out.println(queue.poll()); // Remove the head of this queue from the list
+		System.out.println(queue); // All elements finished to remove
+
+	}
+}

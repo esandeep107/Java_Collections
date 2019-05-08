bc6a0225613173916c02f82f09e60af85bd6a01f
sayaka-tamura <arccycle@gmail.com>
2019-05-09 00:35:01 +0900

    Add MapTest examples

--- test/com/in28minutes/collections/MapTest.java
+++ test/com/in28minutes/collections/MapTest.java
@@ -7,6 +7,8 @@
 package com.in28minutes.collections;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 import java.util.HashMap;
@@ -16,6 +18,7 @@ import org.junit.jupiter.api.Test;
 
 class MapTest {
 
+	// Example 1
 	@Test
 	public void testAssertMap() {
 		Map<String, String> map = new HashMap<>();
@@ -37,7 +40,33 @@ class MapTest {
 
 		// 2. Test size
 		assertThat(map.size(), is(4));
-
 	}
 
+	// Example 2 : mocking
+//	private MySubString mySubString = new MySubString();
+//	@Test
+//	public void testFindOccurences() {
+//		final Map<String, Integer> myResult = mySubString.findOccurences("This This");
+//		final Map<String, Integer> expected = new HashMap<String, Integer>() {
+//			{
+//				put("Thi", 2);
+//				put("his", 2);
+//				put("is ", 1);
+//				// etc
+//			}
+//		};
+//		assertEquals(expected, myResult);
+//	}
+
+	// Example 3
+	@Test
+	public static void assertEqualsMapEpsilon(Map<Object, Double> expected, Map<Object, Double> actual,
+			double epsilon) {
+		assertEquals(expected.size(), actual.size());
+		for (Map.Entry<Object, Double> value : expected.entrySet()) {
+			Double actualValue = actual.get(value.getKey());
+			assertNotNull(actualValue);
+			assertEquals(value.getValue(), actualValue, epsilon);
+		}
+	}
 }

95a546fdd0a7b42ed7812c0a472edc14e7a50e41
sayaka-tamura <arccycle@gmail.com>
2019-05-08 14:53:31 +0900

    Add MapRunner Class

---
+++ src/com/in28minutes/collections/MapRunner.java
@@ -0,0 +1,21 @@
+/**
+ * @author : Sayaka Tamura
+ * May 8, 2019
+ * Udemy Java Collections 3
+ * https://www.youtube.com/watch?v=114BHPiaPGc&list=WL&index=7
+ */
+
+/**
+ * Map:
+ * <Basic>Map provides a data structure based on key-value pair and hashing
+ * <Duplication> While a Map holds two objects per Entry e.g. a key and a value and It may contain duplicate values but keys are always unique. 
+ */
+package com.in28minutes.collections;
+
+public class MapRunner {
+
+	public static void main(String[] args) {
+
+	}
+
+}
